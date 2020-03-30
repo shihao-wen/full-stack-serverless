ba29b11a5b4acacafe2b56477761349d6258b568
Shihao Wen <wenshihao22@gmail.com>
2020-03-30 16:44:42 -0400

    SNS, Auth

--- serverless/src/lambda/http/createImage.ts
+++ serverless/src/lambda/http/createImage.ts
@@ -1,7 +1,9 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
+import * as AWS  from 'aws-sdk'
 import * as uuid from 'uuid'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const s3 = new AWS.S3({
@@ -13,7 +15,7 @@ const imagesTable = process.env.IMAGES_TABLE
 const bucketName = process.env.IMAGES_S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event)
   const groupId = event.pathParameters.groupId
   const validGroupId = await groupExists(groupId)
@@ -21,9 +23,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   if (!validGroupId) {
     return {
       statusCode: 404,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
       body: JSON.stringify({
         error: 'Group does not exist'
       })
@@ -32,18 +31,23 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   const imageId = uuid.v4()
   const newItem = await createImage(groupId, imageId, event)
+
   const url = getUploadUrl(imageId)
+
   return {
     statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
     body: JSON.stringify({
       newItem: newItem,
       uploadUrl: url
     })
   }
-}
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
 
 async function groupExists(groupId: string) {
   const result = await docClient
