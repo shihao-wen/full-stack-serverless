2ecbffe0c9b5a13fd6e6e04da35c83ac10c42772
Shihao Wen <wenshihao22@gmail.com>
2020-04-02 14:09:43 -0400

    init

--- backend/src/lambda/http/createImage.ts
+++ backend/src/lambda/http/createImage.ts
@@ -1,9 +1,7 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 import * as uuid from 'uuid'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const s3 = new AWS.S3({
@@ -15,7 +13,7 @@ const imagesTable = process.env.IMAGES_TABLE
 const bucketName = process.env.IMAGES_S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
-export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event)
   const groupId = event.pathParameters.groupId
   const validGroupId = await groupExists(groupId)
@@ -23,6 +21,9 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
   if (!validGroupId) {
     return {
       statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
       body: JSON.stringify({
         error: 'Group does not exist'
       })
@@ -36,18 +37,15 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
 
   return {
     statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       newItem: newItem,
       uploadUrl: url
     })
   }
-})
-
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+}
 
 async function groupExists(groupId: string) {
   const result = await docClient
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = 'https://.execute-api.us-east-1.amazonaws.com/dev/'
+export const apiEndpoint = '...'
